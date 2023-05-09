80854cc8e681aca10aa1c2c3bfd4a808ef278fc1
Nata Vacheishvili <natashkela@gmail.com>
2023-05-09 18:50:55 -0400

    done

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const functions = require("firebase-functions");
-
+// const functions = require("firebase-functions");
+import * as functions from 'firebase-functions';
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
@@ -7,3 +7,38 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+import express from 'express';
+import bodyParser from "body-parser";
+const app = express();
+app.use(bodyParser.json())
+
+app.get('/', (req,res) => {
+    res.send("Hello World!");
+})
+
+app.get('/user', (req,res) => {
+    res.send("You are getting a user data back!");
+})
+
+app.get('/user', (req,res) => {
+    console.log("This is never going to run");
+    res.send("You cannot get this response back");
+})
+
+app.post('/user', (req, res) => {
+    console.log(req.body);
+    res.send("We created a user with firstname of "+req.body.firstName);
+})
+
+app.delete('/user', (req, res) => {
+    console.log(req.body);
+    res.send("We deleted a user with firstname of "+req.body.firstName);
+})
+
+app.put('/user', (req, res) => {
+    console.log(req.body);
+    res.send("We updated a user with firstname of "+req.body.firstName);
+})
+
+// exports.app = functions.https.onRequest(app);
+export const helloWorld = functions.https.onRequest(app);
